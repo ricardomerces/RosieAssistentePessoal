acb04a35ac6bd7e3e666080dd20a662c63aaa594
ricardomerces <ricardo.merces@gmail.com>
2018-09-13 12:03:58 -0300

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='RosieAssistentePessoal',
+    version='1.0',
+    packages=['rosieAsistentePessoal'],
+    url='https://github.com/ricardomerces/RosieAssistentePessoal',
+    license='MIT',
+    author='RicardoMerces',
+    author_email='ricardo.merces@gmail.com',
+    description='Rosie - Assistente Pessoal'
+)
